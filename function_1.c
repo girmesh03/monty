8dc7085018b615e65359c15edddc6860d72b1e73
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-18 01:54:44 +0300

    function_1.c file is added

---
+++ function_1.c
@@ -0,0 +1,91 @@
+#include "monty.h"
+
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+
+/**
+ * push - pushes an element to the stack
+ * @stack: double pointer to the stack
+ * @line_number: line number of the opcode
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new;
+	char *token;
+	int n;
+
+	token = strtok(NULL, " \n\t");
+	if (((token == NULL) || (token[0] < '0' || token[0] > '9'))
+			&& (token[0] != '-'))
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(token);
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = n;
+	new->prev = NULL;
+	new->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new;
+	*stack = new;
+}
+
+/**
+ * pall - prints all the values on the stack, starting from the top
+ * @stack: double pointer to the stack
+ * @line_number: line number of the opcode
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+
+	(void)line_number;
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+}
+
+/**
+ * pint - prints the value at the top of the stack, followed by a new line
+ * @stack: double pointer to the stack
+ * @line_number: line number of the opcode
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
+
+/**
+ * pop - removes the top element of the stack
+ * @stack: double pointer to the stack
+ * @line_number: line number of the opcode
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(tmp);
+}
