f9909a26367f937d5ffdf238c997a7189f8d6116
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-18 07:23:27 +0300

    function_4.c code changed

--- function_4.c
+++ function_4.c
@@ -79,14 +79,13 @@ void mod(stack_t **stack, unsigned int line_number)
  */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL || stack == NULL)
+	if (!stack || !(*stack))
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 		return;
 	}
-
-	if (__isascii((*stack)->n) == 0)
+	if (isascii((*stack)->n) == 0)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
