f686b7e3c400c450c54deefec45fb8a7bb21db96
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-18 07:16:12 +0300

    function_4.c updated

--- function_4.c
+++ function_4.c
@@ -78,7 +78,7 @@ void mod(stack_t **stack, unsigned int line_number)
  */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL)
+	if (*stack == NULL || stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
