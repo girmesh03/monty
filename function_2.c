20724c691da08f592e2796c4981c53825229d94c
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-19 17:27:37 +0300

    function_2.c is modified

--- function_2.c
+++ function_2.c
@@ -9,17 +9,22 @@ void sub(stack_t **stack, unsigned int line_number);
  * swap - swaps the top two elements of the stack
  * @stack: double pointer to the stack
  * @line_number: line number of the opcode
+ * Return: void
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
+	/* tmp is a pointer to the top of the stack */
 	stack_t *tmp = *stack;
+	/* n is a temporary variable to hold the value of the top of the stack */
 	int n;
 
+	/* if stack is empty or has only one element, print error message */
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	/* swap the values of the top two elements of the stack */
 	n = tmp->n;
 	tmp->n = tmp->next->n;
 	tmp->next->n = n;
@@ -29,6 +34,7 @@ void swap(stack_t **stack, unsigned int line_number)
  * add - adds the top two elements of the stack
  * @stack: double pointer to the stack
  * @line_number: line number of the opcode
+ * Return: void
  */
 void add(stack_t **stack, unsigned int line_number)
 {
@@ -40,7 +46,9 @@ void add(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
+	/* add the top two elements of the stack */
 	tmp->next->n += tmp->n;
+	/* remove the top element of the stack */
 	pop(stack, line_number);
 }
 
@@ -48,9 +56,11 @@ void add(stack_t **stack, unsigned int line_number)
  * nop - does nothing
  * @stack: double pointer to the stack
  * @line_number: line number of the opcode
+ * Return: void
  */
 void nop(stack_t **stack, unsigned int line_number)
 {
+	/* stack and line_number are unused */
 	(void)stack;
 	(void)line_number;
 }
@@ -59,17 +69,22 @@ void nop(stack_t **stack, unsigned int line_number)
  * sub - subtracts the top element of the stack from the second top element
  * @stack: double pointer to the stack
  * @line_number: line number of the opcode
+ * Return: void
  */
 void sub(stack_t **stack, unsigned int line_number)
 {
+	/* tmp is a pointer to the top of the stack */
 	stack_t *tmp = *stack;
 
+	/* if stack is empty or has only one element, print error message */
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
+	/* subtract the top element of the stack from the second top element */
 	tmp->next->n -= tmp->n;
+	/* remove the top element of the stack */
 	pop(stack, line_number);
 }
