b0e24ef79e03d31b4ed8c6268487e3cdb3e8915a
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-19 17:00:00 +0300

    main.c function array modified

--- run_monty.c
+++ run_monty.c
@@ -19,6 +19,19 @@ void opcode(stack_t **stack, char *str, unsigned int line_cnt)
 	instruction_t opcodes[] = {
 		{"push", push},
 		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{"sub", sub},
+		{"div", divi},
+		{"mul", mul},
+		{"mod", mod},
+		{"pchar", pchar},
+		{"pstr", pstr},
+		{"rotl", rotl},
+		{"rotr", rotr},
 		{NULL, NULL}};
 
 	/* check mode stack or queue */
