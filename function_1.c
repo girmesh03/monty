aefedaf26cf3728fdbf485b2276ebe82904faad3
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-19 16:56:38 +0300

    pint and pop functions added

--- function_1.c
+++ function_1.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/* In a file function_1.c */
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+
 /**
  * push - pushes an element to the stack
  * @stack: pointer to the top of the stack
@@ -63,4 +69,48 @@ void pall(stack_t **stack, unsigned int line_cnt)
 		/* set current to next node */
 		current = current->next;
 	}
+}
+
+/**
+ * pint - prints the value at the top of the stack, followed by a new line
+ * @stack: double pointer to the stack
+ * @line_cnt: line number of the opcode
+ */
+void pint(stack_t **stack, unsigned int line_cnt)
+{
+	/* if stack is empty, print error message */
+	/* and exit with status EXIT_FAILURE */
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_cnt);
+		exit(EXIT_FAILURE);
+	}
+	/* print value at top of stack */
+	printf("%d\n", (*stack)->n);
+}
+
+/**
+ * pop - removes the top element of the stack
+ * @stack: double pointer to the stack
+ * @line_cnt: line number of the opcode
+ */
+void pop(stack_t **stack, unsigned int line_cnt)
+{
+	/* set tmp to top of stack */
+	stack_t *tmp = *stack;
+
+	/* if stack is empty, print error message */
+	/* and exit with status EXIT_FAILURE */
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_cnt);
+		exit(EXIT_FAILURE);
+	}
+	/* set top of stack to next node */
+	*stack = (*stack)->next;
+	/* if stack is not empty, set prev of new top to NULL */
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	/* free top of stack */
+	free(tmp);
 }
